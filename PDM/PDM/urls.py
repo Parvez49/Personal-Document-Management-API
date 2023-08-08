65f532f5ea9cbc634671faa3fae8b5b5af40f5e0
Parvez Hossen <72366747+Parvez49@users.noreply.github.com>
2023-08-08 21:48:07 +0600

    Update urls.py

--- PDM/PDM/urls.py
+++ PDM/PDM/urls.py
@@ -27,7 +27,7 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Snippets API",
+      title="PDM API",
       default_version='v1',
       description="Test description",
    ),
